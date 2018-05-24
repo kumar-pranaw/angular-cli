cfdc96346afacdd7ac70874722114450655d87a1
pranavtechnossus <39586967+pranavtechnossus@users.noreply.github.com>
2018-05-24 15:42:39 +0530

    added new component
    created new component using cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
