92f89c99d9a4f48630841b00a12f2a7a68b0159d
pranavtechnossus <39586967+pranavtechnossus@users.noreply.github.com>
2018-05-24 15:55:05 +0530

    added nav bar
    added new component navbar and added some conent in home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
