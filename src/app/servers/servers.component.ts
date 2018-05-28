e5b30de1ec9ce82ff089b41f4f4e6009c18b1a08
pranavtechnossus <39586967+pranavtechnossus@users.noreply.github.com>
2018-05-28 12:22:29 +0530

    2nd Assignment
    Tasks
    1) Using string interpolation
    2) Used 2 way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer=false;
   serverCreationStatus= 'No server was created!! ';
-  serverName='';
+  serverName='TestServer';
 
   constructor() {
     setTimeout(() => {
@@ -20,7 +20,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer(){
-    this.serverCreationStatus='server was created!! Yippee';
+    this.serverCreationStatus='server was created!! Yippee'+ this.serverName;
   }
   onUpdateServerName(event: Event){
   this.serverName = (<HTMLInputElement>event.target).value;
